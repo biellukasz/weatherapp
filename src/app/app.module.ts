2e2153245ab6ea1fad06cb1527923382df9095bf
lbiel <lbiel@oeconnection.com>
2021-11-28 17:59:14 +0100

    removed unused side nav adjusted formatting for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CurrentWeatherComponent} from './current-weather/current-weather.component';
 import {WeatherService} from "./service/weather/weather.service";
 import {HttpClientModule} from "@angular/common/http";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from "@angular/material/card";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import { CitySearchComponent } from './city-search/city-search.component';
+import {CitySearchComponent} from './city-search/city-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrentWeatherComponent,
-    SideNavComponent,
     CitySearchComponent
   ],
   imports: [
@@ -50,4 +48,5 @@ import { CitySearchComponent } from './city-search/city-search.component';
   providers: [WeatherService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
